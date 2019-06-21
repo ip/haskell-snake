ffd74efabf2ae69e9c18dc6bedc73c6f8b203edd
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-06-22 02:53:01 +0400

    Install HsCharm, move code to Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
 module Main where
 
-import Lib
+import Control.Concurrent
+import Control.Monad
+import HsCharm
 
 main :: IO ()
-main = someFunc
+
+{--
+-- A REPL which multiplies numbers by 2
+someFunc = runStep
+
+runStep :: IO ()
+runStep = do
+    putStrLn "Enter a number or \"q\" to quit: "
+    line <- getLine
+    if line == "q" then
+        return ()
+    else do
+        let x = read line :: Integer
+            in putStrLn $ show x ++ " * 2 = " ++ (show $ x * 2)
+        runStep
+--}
+
+-- someFunc = tick 10
+
+-- tick :: Integer -> IO ()
+-- tick step = do
+--     print step
+--     threadDelay 100000
+--     when (step > 0) $ tick (step - 1)
+
+main = print 1234
