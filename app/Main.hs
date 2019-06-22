c42f0c3885fef3989d5b677d212ef8c8f34bfa21
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-06-22 04:10:49 +0400

    Refactor

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,9 @@ data Vec2 = Vec2 { x :: Int
 data GameState = GameState { position :: Vec2
                            , velocity :: Vec2 }
 
-frameDelay = 300 * 1000 -- Microseconds
+data Inputs = Inputs { screenSize :: Vec2 }
+
+frameDelay = 100 * 1000 -- Microseconds
 initialState = GameState { position = Vec2 0 0
                          , velocity = Vec2 1 1 }
 
@@ -30,17 +32,35 @@ initGame = startCharm
 
 runFrame :: GameState -> IO GameState
 runFrame state = do
-    -- w <- getWidth
-    -- h <- getHeight
-    clearScreen
-    let Vec2 x y = position state in moveCursor x y
-    blotChar 'o'
-    return $ updateState state
+    renderFrame state
+    inputs <- getInputs
+    let nextState = updateState inputs state in return nextState
 
-updateState :: GameState -> GameState
-updateState state = GameState { position = (position state) `addVec2` (velocity state)
-                              , velocity = velocity state }
+updateState :: Inputs -> GameState -> GameState
+updateState inputs s =
+    GameState {
+        position = (position s) `addVec2` (velocity s),
+        velocity = velocity s
+    }
 
 addVec2 :: Vec2 -> Vec2 -> Vec2
 a `addVec2` b = Vec2 { x = (x a) + (x b)
                      , y = (y a) + (y b) }
+
+-- Side effects
+
+renderFrame :: GameState -> IO ()
+renderFrame state = do
+    clearScreen
+    let Vec2 x y = position state in moveCursor x y
+    blotChar 'o'
+
+getInputs :: IO Inputs
+getInputs = fmap Inputs getScreenSize
+
+{-# ANN module "HLint: Use <$>" #-}
+getScreenSize :: IO Vec2
+getScreenSize = do
+    w <- getWidth
+    h <- getHeight
+    return $ Vec2 w h
