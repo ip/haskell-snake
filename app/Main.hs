7711c59ad58647748157df8e8d1537fc53a91f04
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-06-23 16:43:12 +0400

    Move Vec2 into separate module

--- app/Main.hs
+++ app/Main.hs
@@ -4,30 +4,7 @@ import Control.Concurrent
 import Control.Monad
 import System.Random
 import HsCharm
-
-
--- Vec2
-
-data Vec2 = Vec2 {
-    x :: Int,
-    y :: Int
-} deriving (Show)
-
-instance Num Vec2 where
-    a + b = Vec2 (x a + x b) (y a + y b)
-    a * b = Vec2 (x a * x b) (y a * y b)
-    a - b = Vec2 (x a - x b) (y a - y b)
-    -- TODO: remove duplication with signum by instancing Functor and using fmap
-    abs a = Vec2 (abs $ x a) (abs $ y a)
-    signum a = Vec2 (signum $ x a) (signum $ y a)
-    fromInteger a = let b = fromInteger a in Vec2 b b
-
-infixl 7 //
-(//) :: Vec2 -> Int -> Vec2
-(//) v m = Vec2 {
-    x = x v `div` m,
-    y = y v `div` m
-}
+import Vec2
 
 
 data GameState = GameState {
